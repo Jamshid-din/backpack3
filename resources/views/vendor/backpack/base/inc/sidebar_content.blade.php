4827650e7b1f5491584a5ff00b253e5f86d55e0c
Jamshiddin Ismailov <jamshiddin.ismailov@gmail.com>
2023-03-09 18:49:08 +0500

    small fix on menu

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,24 +2,26 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <!-- Users, Roles, Permissions -->
-<li class="nav-item nav-dropdown">
-  <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> {{ trans('custom.menu_authentication') }}</a>
-  <ul class="nav-dropdown-items">
+@if (backpack_user()->can('users list') || backpack_user()->can('roles list') || backpack_user()->can('permissions list'))
+  <li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> {{ trans('custom.menu_authentication') }}</a>
+    <ul class="nav-dropdown-items">
 
-      @if (backpack_user()->can('users list'))
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>{{ trans('custom.menu_users') }}</span></a></li>
-      @endif
+        @if (backpack_user()->can('users list'))
+          <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>{{ trans('custom.menu_users') }}</span></a></li>
+        @endif
 
-      @if (backpack_user()->can('roles list'))
-      <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>{{ trans('custom.menu_roles') }}</span></a></li>
-      @endif
+        @if (backpack_user()->can('roles list'))
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>{{ trans('custom.menu_roles') }}</span></a></li>
+        @endif
 
-      @if (backpack_user()->can('permissions list'))
-      <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>{{ trans('custom.menu_permissions') }}</span></a></li>
-      @endif
+        @if (backpack_user()->can('permissions list'))
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>{{ trans('custom.menu_permissions') }}</span></a></li>
+        @endif
 
-  </ul>
-</li> 
+    </ul>
+  </li> 
+@endif
 @if (backpack_user()->can('orders list'))
   <li class="nav-item"><a class="nav-link" href="{{ backpack_url('orders') }}"><i class="nav-icon la la-question"></i> {{ trans('custom.menu_orders') }}</a></li>
 @endif
